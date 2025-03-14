265051dbb44f2b399c1780bd68255ba3821b3e64
Salatnicha <k.zaitsev@tushitesvet.ru>
2025-03-14 14:41:01 +0300

    Сделан один спавнер

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -2,5 +2,9 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
+	[SerializeField] private Enemy _enemy;
+	[SerializeField] private Target _target;
 
+	public Enemy Enemy { get { return _enemy; } private set { _enemy = value; } }
+	public Target Target { get { return _target; } private set { _target = value; } }
 }
--- Assets/Scripts/SpawnerEnemy.cs
+++ Assets/Scripts/SpawnerEnemy.cs
@@ -3,9 +3,7 @@ using UnityEngine;
 
 public class SpawnerEnemy : MonoBehaviour
 {
-	[SerializeField] private Enemy _enemyPrefab;
 	[SerializeField] private SpawnPoint[] _points;
-	[SerializeField] private Target[] _targets;
 	[SerializeField][Min(0.01f)] private float _rate = 2;
 
 	private void Start()
@@ -22,10 +20,10 @@ public class SpawnerEnemy : MonoBehaviour
 		while (isSpawn)
 		{
 			int randomPonint = Random.Range(0, _points.Length);
-			Enemy enemy = Instantiate(_enemyPrefab, _points[randomPonint].transform.position, Quaternion.identity);
+			SpawnPoint currentPoint = _points[randomPonint];
+			Enemy enemy = Instantiate(currentPoint.Enemy, currentPoint.transform.position, Quaternion.identity);
 
-			int randomTarget = Random.Range(0, _targets.Length);
-			enemy.Initializate(_targets[randomTarget]);
+			enemy.Initializate(currentPoint.Target);
 
 			yield return rateWait;
 		}
